ed98d0c561939633cf4c7578e2edd69d8fc7c4e2
Lameck <lvicarson@gmail.com>
2015-04-09 15:01:48 +0300

    case statement
    sms case statement for domestic animals

--- practice.php
+++ practice.php
@@ -7,6 +7,22 @@
 //$name = "steve";
 //echo $name;
 
+$phone_no = array("123132131","123123123");
+$message = "Download songs from wwww.sing.com";
+//I want to reach all my customers via SMS
+//noun: customers, SMS
+//Verb: reach
+//Customer: phone_number
+//SMS:message, recipients
+//actors: customer, owner (admin), staff,
+//admin, staff should have the ability to send a message, upload a number
 
-
+function showOutput(output){
+  if(is_array(output)){
+    print_r($output);
+    }
+ else{
+     echo $output;
+    }
+}
 ?>
